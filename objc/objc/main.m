c51505fa9b037b5fd9f70490bb6c3b9b563599ef
OTO <7h070@naver.com>
2011-02-15 12:39:58 +0000

    korean lang test

--- objc/objc/main.m
+++ objc/objc/main.m
@@ -1,8 +1,10 @@
 //
 //  main.m
 //  objc
-//
+//  github : git@github.com:7h070/objc.git
+
 //  Created by OTO on 11. 2. 15..
+//  7h070@naver.com
 //  Copyright 2011 kcom. All rights reserved.
 //
 
@@ -11,12 +13,13 @@
 int main (int argc, const char * argv[])
 {
 
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    //NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
     // insert code here...
     NSLog(@"Hello, World!");
+    NSLog(@"한글 테스트!\n");
 
-    [pool drain];
+    //[pool drain];
     return 0;
 }
 
